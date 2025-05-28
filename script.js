a76b100429cc6dea53e1ea4e85b8352c90786bbe
itsvishalyadav <yadavishal259@gmail.com>
2025-05-28 17:56:30 +0530

    Made a website for Rock Paper Scissors using CSS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let humanScore = 0
 let compScore = 0
 
+  document.getElementById("yscr").textContent = humanScore;
+  document.getElementById("cscr").textContent = compScore;
+
 function getComputerChoice() {
   const num = Math.random()
   let choice
@@ -17,14 +20,11 @@ function getComputerChoice() {
   return choice.toLowerCase()
 }
 
-function getHumanChoice() {
-  let input = prompt('Enter your move : ')
-  return input.toLowerCase()
-}
-
 function playRound(human, computer) {
+  let act="";
   console.log('You chose ' + human)
   console.log('Computer chose ' + computer)
+  act+='You chose ' + human+'<br>Computer chose ' + computer
 
   if (
     (human == 'rock' && computer == 'paper') ||
@@ -33,6 +33,7 @@ function playRound(human, computer) {
   ) {
     compScore++
     console.log('You lose, Computer WON!!')
+    act+='<br><br>You lose, Computer WON!!'
   } else if (
     (computer == 'rock' && human == 'paper') ||
     (computer == 'paper' && human == 'scissors') ||
@@ -40,29 +41,48 @@ function playRound(human, computer) {
   ) {
     humanScore++
     console.log('You WON!!')
+    act+='<br><br>You WON!!'
   } else if (
     (computer == 'rock' && human == 'rock') ||
     (computer == 'paper' && human == 'paper') ||
     (computer == 'scissors' && human == 'scissors')
   ) {
     console.log('Round tie!!')
+    act+='<br><br>Round tie!!'
   }
+  document.getElementById("yscr").textContent = humanScore;
+  document.getElementById("cscr").textContent = compScore;
+  document.getElementById("actions").innerHTML = act;
   console.log('Your Score: ' + humanScore + ' Computer Score: ' + compScore)
 }
 
 function getWinner() {
-  let winner='COMPUTER' ;
-  if(humanScore>compScore){
-    winner='YOU';
+  let winner = ''
+  if (humanScore > compScore) {
+    alert('Winner is YOU')
+  } else if (humanScore < compScore) {
+    alert('Winner is COMPUTER' + winner)
   }
-  console.log('Winner : ' + winner);
 }
 
 function playGameOf5() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice())
+  let clicks = 0
+  if (clicks >= 5) {
+    return getWinner()
+  } else {
+    const buttons = document.querySelectorAll('button')
+
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const playerChoice = button.id
+        const computerChoice = getComputerChoice()
+
+        playRound(playerChoice, computerChoice)
+        clicks++
+        console.log(clicks)
+      })
+    })
   }
-  getWinner();
 }
 
-playGameOf5();
+playGameOf5()
